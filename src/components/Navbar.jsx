fc25e54f2b681b3fc75ad47a7d7ca5845340939a
sam <sammybonfanti@gmail.com>
2022-06-08 13:26:15 -0700

    initial setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,6 @@
+import { useState } from 'react'
+
+function Navbar() {
+	return <div>Navbar</div>
+}
+export default Navbar
---
+++ src/routes/CoinPage.jsx
@@ -0,0 +1,6 @@
+function CoinPage() {
+  return (
+    <div>CoinPage</div>
+  )
+}
+export default CoinPage
